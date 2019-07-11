d059cd90982021d1a68f387e54f7ba930a3fdd3c
Michael Penrod <michael.penrod@vivint.com>
2019-07-11 16:17:46 -0600

    Initial

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { ApplicationModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  await app.listen(3000);
+  await app.listen(3000, () => console.info(`Server running on port 3000`));
 }
+
 bootstrap();
